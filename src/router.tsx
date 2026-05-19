3cc72b6e06c219f7cfebe54cd62daf3552a7af07
SevTify[404] <gametech228@gmail.com>
2026-05-19 08:51:08 +0000

    Ajout de nouveaux composants et fonctionnalités pour améliorer l'interface utilisateur. Création d'un composant `Skeleton` pour les états de chargement, d'un composant `Separator` pour une meilleure séparation visuelle, et d'un composant `Tooltip` pour des informations contextuelles. Introduction d'un hook `useIsMobile` pour détecter les appareils mobiles et ajuster l'interface en conséquence. Mise à jour de la configuration de la bibliothèque d'icônes en remplaçant `hugeicons` par `lucide-react` dans le fichier `components.json`. Ajout d'un fichier `SidebarScopes` pour gérer les autorisations d'accès dans la barre latérale. Ces modifications visent à enrichir l'expérience utilisateur et à rendre l'application plus réactive et intuitive.

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,42 @@ import {
 } from 'react-router'
 
 import App  from "./App.tsx";
+import AppLayout from "@/layouts/AppLayout.tsx";
+import Placeholder from "@/components/Placeholder.tsx";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         Component: App,
     },
+    {
+        path: '/admin',
+        Component: AppLayout,
+        children: [
+            {
+                path: 'dashboard',
+                Component: Placeholder,
+            },
+            {
+                path: 'produits',
+                Component: Placeholder,
+            },
+            {
+                path: 'commandes',
+                Component: Placeholder,
+            },
+            {
+                path: 'clients',
+                Component: Placeholder,
+            },
+            {
+                path: 'logs',
+                Component: Placeholder,
+            },
+            {
+                path: 'settings',
+                Component: Placeholder,
+            },
+        ]
+    }
 ])
