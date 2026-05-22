0ad8eaf89ec28b15ebeca126846f02ee5c0f13b1
SevTify[404] <gametech228@gmail.com>
2026-05-22 05:21:34 +0000

    refactor: Mise à jour des composants pour utiliser la bibliothèque d'icônes Lucide. Remplacement des icônes Hugeicons par des icônes Lucide dans les fichiers `breadcrumb.tsx`, `button.tsx`, `dropdown-menu.tsx`, `input.tsx`, `select.tsx`, `sheet.tsx`, et `sidebar.tsx`. Amélioration de la cohérence visuelle et de la performance en utilisant des composants d'icônes plus légers. Ajustement des styles CSS pour assurer une intégration harmonieuse avec les nouvelles icônes.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import {CheckmarkCircle02Icon} from "@hugeicons/core-free-icons";
 import {HugeiconsIcon} from "@hugeicons/react";
 import useTheme from "@/hooks/useTheme.ts";
 import {useNavigate} from "react-router";
-import { PATHS_MAPPING } from "@/routing/paths-mapping.ts";
+import {FULL_ADMIN_ROUTES_MAPPING} from "@/routing/paths-mapping.ts";
 
 function Index() {
     const {toggleTheme} = useTheme();
@@ -18,7 +18,7 @@ function Index() {
                     position: 'top-right',
                     icon: <HugeiconsIcon icon={CheckmarkCircle02Icon} />,
                 });
-                navigate(PATHS_MAPPING.ADMIN);
+                navigate(FULL_ADMIN_ROUTES_MAPPING.DASHBOARD);
             }}>
                 Test Vers Admin
             </Button>
