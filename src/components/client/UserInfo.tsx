5d63fb607e034e57bbe5005957d7c74b1828d38f
SevTify[404] <gametech228@gmail.com>
2026-06-08 10:22:03 +0000

    feat: Réorganisation du composant `UserInfo` pour améliorer la présentation des informations utilisateur. Introduction d'une structure de données pour gérer les sections "Profile", "Work" et "Billing", permettant une navigation plus claire entre les différentes catégories d'informations. Mise à jour de la logique d'affichage pour intégrer des icônes et des titres descriptifs, tout en remplaçant l'ancienne logique de rendu par un composant `InfoContent` plus flexible. Ces modifications visent à enrichir l'expérience utilisateur en offrant une interface plus intuitive et structurée pour l'affichage des données personnelles.

--- src/components/client/UserInfo.tsx
+++ src/components/client/UserInfo.tsx
@@ -4,18 +4,105 @@ import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar.tsx";
 import {useUserAccount} from "@/hooks/react-queries-hooks/useUserAccount.ts";
 import {extractUserInitials} from "@/routing/utils/extractor.ts";
 import type {MeResponse} from "@/types/schemas/authSchemas.ts";
-import {
-    Card,
-    CardContent,
-    CardHeader,
-} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardHeader,} from "@/components/ui/card.tsx";
 import {Badge} from "@/components/ui/badge.tsx";
 import DisconnectAlertDialog from "@/components/shared/DisconnectAlertDialog.tsx";
+import {Briefcase, Building, Building2, House, Info, type LucideIcon, Mail, Phone} from "lucide-react";
+import {useMemo, useState} from "react";
+
+const infos = {
+    PROFILE: {
+        title: "Profile",
+        items: [
+            {
+                title: "Informations Personnelles",
+                items: [
+                    {
+                        title: "Nom Complet",
+                        content: (userInfos: MeResponse) => `${userInfos?.firstName} ${userInfos?.lastName}`,
+                        icon: Info
+                    },
+                    {
+                        title: "Nom d'utilisateir",
+                        content: (userInfos: MeResponse) => userInfos?.username || "Non renseigné",
+                        icon: Info
+                    }
+                ]
+            },
+            {
+                title: "Contacts Détails",
+                items: [
+                    {
+                        title: "Email",
+                        content: (userInfos: MeResponse) => userInfos?.email,
+                        icon: Mail
+                    },
+                    {
+                        title: "Numéro de téléphone",
+                        content: (userInfos: MeResponse) => userInfos?.phone || "Non renseigné",
+                        icon: Phone
+                    }
+                ]
+            }
+        ]
+    },
+    WORK: {
+        title: "Work",
+        items: [
+            {
+                title: "Infos Travail",
+                items: [
+                    {
+                        title: "Poste",
+                        icon: Briefcase,
+                        content: (userInfos) => "Non renseigné"
+                    },
+                    {
+                        title: "Département",
+                        icon: Building,
+                        content: () => 'Non renseigné'
+                    },
+                ]
+            },
+            {
+                title: "Localisation",
+                items: [
+                    {
+                        title: "Ville",
+                        content: () => "Non renseigné",
+                        icon: Building2
+                    },
+                    {
+                        title: "Adresse",
+                        content: () => "Non renseigné",
+                        icon: House
+                    },
+                    {
+                        title: "Etat",
+                        content: () => "Non renseigné",
+                        icon: House
+                    }
+                ]
+            }
+        ]
+    },
+    BILLING: {
+        title: "Billing",
+        items: []
+    }
+}
+
+const Tabs = {
+    PROFILE: infos.PROFILE,
+    WORK: infos.WORK,
+    BILLING: infos.BILLING
+}
 
 export default function UserInfo() {
     const {user} = useUserAccount();
 
-
+    const [activeTab, setActiveTab] = useState<InfoScope>(Tabs.PROFILE);
+    const tabs = useMemo(() => Object.values(Tabs).map((tab) => tab.title), []);
     const userFullname = `${user?.firstName} ${user?.lastName}`;
     const userInitials = extractUserInitials(userFullname);
     return (
@@ -27,45 +114,51 @@ export default function UserInfo() {
             <span className="font-medium text-2xl mt-1">{userFullname}</span>
             <Badge variant="outline" className="my-1">{user?.role.toUpperCase()}</Badge>
             <span className="text-sm text-muted-foreground mb-1">{user?.email}</span>
-            <AnimatedTabs tabs={["Profile", "Work", "Billing"]}/>
+            <AnimatedTabs tabs={tabs}/>
             <Separator className="my-2"/>
-            <ProfileInfo userInfos={user as MeResponse}/>
+            <InfoContent activeTab={activeTab} userInfos={user as MeResponse}/>
             <Separator className="my-2"/>
             <DisconnectAlertDialog buttonFullWidth/>
         </div>
     )
 }
 
-const profilInfoCards = [
-    {
-        title: "Nom Complet",
-        content: (userInfos: MeResponse) => `${userInfos?.firstName} ${userInfos?.lastName}`
-    },
-    {
-        title: "Email",
-        content: (userInfos: MeResponse) => userInfos?.email
-    },
-    {
-        title: "Rôle",
-        content: (userInfos: MeResponse) => userInfos?.role
-    },
-    {
-        title: "Genre",
-        content: (userInfos: MeResponse) => userInfos?.gender
-    }
-]
-function ProfileInfo({userInfos} : Readonly<{ userInfos: MeResponse }>) {
+interface Section {
+    title: string;
+    items: ScalarInfo[];
+}
+
+interface InfoScope {
+    title: string;
+    items: Section[];
+}
+
+interface ScalarInfo {
+    title: string;
+    content: (userInfos: MeResponse) => string;
+    icon?: LucideIcon
+}
+
+
+function InfoContent({userInfos, activeTab}: Readonly<{ activeTab: InfoScope, userInfos: MeResponse }>) {
     return (
-        <div className="grid grid-cols-2 gap-3 items-center justify-center my-2 w-full ">
-            {profilInfoCards.map((card) => (
-                <Card key={card.title} className="w-full">
-                    <CardHeader className="font-medium text-muted-foreground">
-                        {card.title}
-                    </CardHeader>
-                    <CardContent className="text-lg overflow-hidden text-ellipsis">
-                        {card.content(userInfos)}
-                    </CardContent>
-                </Card>
+        <div>
+            {activeTab.items.map((scope) => (
+                <div key={scope.title} className="my-4">
+                    <h3 className="text-muted-foreground">{scope.title.toUpperCase()}</h3>
+                    <div className="grid grid-cols-2 gap-3 items-center justify-center my-2 w-full">
+                        {scope.items.map((card) => (
+                            <Card key={card.title} className="w-full">
+                                <CardHeader className="font-medium text-muted-foreground">
+                                    {card.title}
+                                </CardHeader>
+                                <CardContent className="text-lg overflow-hidden text-ellipsis">
+                                    {card.content(userInfos)}
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </div>
+                </div>
             ))}
         </div>
     )
