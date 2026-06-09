1835f2504368b1bd80f742694622dc78a8e83634
SevTify[404] <gametech228@gmail.com>
2026-06-09 00:16:08 +0000

    feat: Simplification de l'animation au niveau de UserInfo

--- src/components/client/UserInfo.tsx
+++ src/components/client/UserInfo.tsx
@@ -69,9 +69,9 @@ export default function UserInfo() {
             <AnimatePresence mode="wait">
                 <motion.div
                     key={activeTab.title}
-                    initial={{opacity: 0, y: 10}}
-                    animate={{opacity: 1, y: 0}}
-                    exit={{opacity: 0, y: -10}}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
                     transition={{duration: 0.25, ease: "easeOut"}}
                     className="w-full"
                 >
